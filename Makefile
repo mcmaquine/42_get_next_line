ea3b6f7f6fb67d1f207c3f6e243914bb24879afb
mcmaquine <mcmaquine@gmail.com>
2025-08-03 17:35:29 -0300

    bulding next line

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 n=42
 
-WFLAGS=-Wall -Wextra -Werror -D BUFFER_SIZE=$(n) TFILE="testfile"
+WFLAGS=-Wall -Wextra -Werror -DBUFFER_SIZE=$(n) -DTFILE="testfile"
 
 COMPILER=cc
 
-SRC=get_next_line.c main.c
+SRC=get_next_line.c
 
-a:	$(SRC:.c=.o)
-	$(COMPILER) $(WFLAGS) -c -g -o $@
+a:	$(SRC)
+	$(COMPILER) $(WFLAGS) -g $< -o $@
 
 all:	a
 
--- get_next_line.c
+++ get_next_line.c
@@ -12,15 +12,17 @@
 
 #include "get_next_line.h"
 
-static size_t count_char(char *buf)
+/*static size_t count_char(char *buf)
 {
 	size_t cnt;
 
+	cnt = 0;
 	while(*buf && *buf != '\n')
 	{
 		cnt++;
 		buf++;
 	}
+	return (cnt);
 }
 
 static	void	buff_clear(char *buf)
@@ -30,13 +32,14 @@ static	void	buff_clear(char *buf)
 		*buf = '\0';
 		buf++;
 	}
-}
+}*/
 
 static	char	*get_line(char *buf, size_t s)
 {
 	char	*line;
 
-	line = (char *)malloc(sizeof(char) * s);
+	line = (char *)malloc(sizeof(char) * (s + 1));
+	line[s] = '\0';
 	while(--s)
 	{
 		line[s] = buf[s];
@@ -48,10 +51,39 @@ static	char	*get_line(char *buf, size_t s)
 char	*get_next_line(int fd)
 {
 	ssize_t		read_bytes;
-	static char	buf[BUFFER_SIZE];
-	size_t		line_len;
+	char		c[BUFFER_SIZE];
+	size_t		len;
 
-	read_bytes = read(fd, buf, BUFFER_SIZE);
-	line_len = count_char(*buf);
-	return (get_line(buf, line_len));
+	len = 0;
+	read_bytes = read(fd, &c[len], 1);
+	while (c != '\n' && read_bytes > 0 && len < BUFFER_SIZE)
+	{
+		len++;
+		read_bytes = read(fd, &c[len], 1);
+	}
+	
+	return (get_line(buf, len));
+}
+
+#include <stdio.h>
+#include <fcntl.h>
+int	main()
+{
+	char	*file = "testfile";
+	int		fd = open(file, O_RDONLY);
+	char	*line;
+	int		n = 5;
+
+	if (fd > 2)
+	{
+		line = get_next_line(fd);
+		while (n)
+		{
+			printf("%s", line);
+			free(line);
+			line = get_next_line(fd);
+			n--;
+		}
+		close(fd);
+	}
 }
