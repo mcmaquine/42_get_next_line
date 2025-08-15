a5e33396be5c8c6afd3299906c979f9ad66137b8
Matheus Maquine <mmaquine@c2r5p1.42sp.org.br>
2025-08-15 16:33:00 -0300

    bonus done

---
+++ get_next_line_bonus.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaquine <mmaquine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 17:47:05 by mmaquine          #+#    #+#             */
+/*   Updated: 2025/08/15 16:14:38 by mmaquine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*join(char *bucket, char *buf)
+{
+	char	*jn;
+
+	jn = ft_strjoin(bucket, buf);
+	free(bucket);
+	return (jn);
+}
+
+char	*update_bucket(char *bucket)
+{
+	char	*n_pos;
+	size_t	size;
+	char	*new_bucket;
+
+	if (!bucket)
+		return (NULL);
+	if (!bucket[0])
+	{
+		free(bucket);
+		return (NULL);
+	}
+	n_pos = ft_strchr(bucket, '\n');
+	if (n_pos)
+	{
+		size = ft_strlen(bucket) - (n_pos - bucket + 1);
+		new_bucket = ft_substr(n_pos, 1, size);
+		free(bucket);
+		return (new_bucket);
+	}
+	free(bucket);
+	return (NULL);
+}
+
+char	*read_to_bucket(int fd, char *bucket)
+{
+	char		*buf;
+	ssize_t		bytes_read;
+
+	buf = (char *)ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (!buf)
+		return (NULL);
+	if (!bucket)
+		bucket = (char *)ft_calloc(1, sizeof(char));
+	bytes_read = read(fd, buf, BUFFER_SIZE);
+	while (bytes_read > 0)
+	{
+		buf[bytes_read] = '\0';
+		bucket = join(bucket, buf);
+		if (ft_strchr(bucket, '\n'))
+			break ;
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+	}
+	if (bytes_read == -1)
+	{
+		free(buf);
+		free(bucket);
+		return (NULL);
+	}
+	free(buf);
+	return (bucket);
+}
+
+/*
+Return a string which its last char is '\n' terminatted with '\0'
+*/
+char	*get_a_line(char *bucket)
+{
+	char	*line;
+	char	*n_pos;
+	size_t	size;
+
+	n_pos = ft_strchr(bucket, '\n');
+	if (n_pos)
+		size = n_pos - bucket + 1;
+	else
+		size = ft_strlen(bucket);
+	line = ft_substr(bucket, 0, size);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*bucket[FD_MAX];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	bucket[fd] = read_to_bucket(fd, bucket[fd]);
+	if (!bucket[fd])
+		return (NULL);
+	line = get_a_line(bucket[fd]);
+	bucket[fd] = update_bucket(bucket[fd]);
+	return (line);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmaquine <mmaquine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 11:04:02 by mmaquine          #+#    #+#             */
+/*   Updated: 2025/08/15 16:08:34 by mmaquine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <unistd.h>
+# define FD_MAX 1024
+
+char	*join(char *bucket, char *buf);
+char	*update_bucket(char *bucket);
+char	*read_to_bucket(int fd, char *bucket);
+char	*get_a_line(char *bucket);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strchr(const char *s, int c);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*get_next_line(int fd);
+#endif
